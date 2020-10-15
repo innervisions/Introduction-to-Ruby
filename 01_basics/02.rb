795fc3cf7b6819512676bebde13a9095398f7e3f
Raymond Saade <raymond.saade@gmail.com>
2020-10-14 22:23:44 -0400

    Initial Commit

---
+++ 01_basics/02.rb
@@ -0,0 +1,11 @@
+number = 1689
+
+thousands = number / 1000
+hundreds = number % 1000 / 100
+tens = number % 100 / 10
+ones = number % 10
+
+puts "thousands: #{thousands}"
+puts "hundreds: #{hundreds}"
+puts "tens: #{tens}"
+puts "ones: #{ones}"
---
+++ 01_basics/03.rb
@@ -0,0 +1,9 @@
+movies = {  :brick => 2005,
+            :birdcage => 1996,
+            :labyrinth => 1986,
+            :starship_troopers => 1997 }
+
+puts movies[:brick]
+puts movies[:birdcage]
+puts movies[:labyrinth]
+puts movies[:starship_troopers]
